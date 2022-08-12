ff189bcdff0cba3952b96437ce88c0e45df9df2b
Nadim <nahlenadim@gmail.com>
2022-08-12 19:20:50 +0300

    max sum of sub array

---
+++ python/suabarray2.py
@@ -0,0 +1,13 @@
+def maxarray(array,n):
+    maximum = array[0]
+    res = array[0];
+    for i in range(1,n):
+        maximum = max(maximum + array[i], array[i])
+        res = max(res,maximum)
+    print(res)
+
+array=[1,-2,5,-9,-3-3-3,6]
+n = len(array);
+
+maxarray(array,n)
+
