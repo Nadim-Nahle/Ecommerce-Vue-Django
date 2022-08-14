3ec7fac5b99feb774cf5c74f9d56af352b9e72de
Nadim <nahlenadim@gmail.com>
2022-08-14 16:10:01 +0300

    Rotating List

---
+++ python/rotatingList.py
@@ -0,0 +1,14 @@
+#LINEAR SEARCH
+def rotated(arr,arr1):
+    arr.sort();
+    t = arr[0];
+    for i in range(len(arr1)):
+        if arr1[i] == t:
+            return i
+
+arr=[5,6,9,0,2,3,4]
+arr1=[5,6,9,0,2,3,4]
+
+print(rotated(arr,arr1))
+
+
