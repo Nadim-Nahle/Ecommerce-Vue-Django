ea54828f09e4b70f69fefa3186e6d7513d072a71
Nadim <nahlenadim@gmail.com>
2022-08-28 14:31:45 +0300

    Backend-Django Create- Category Model

--- FinalProject2/backend-django/ecommerce/product/models.py
+++ FinalProject2/backend-django/ecommerce/product/models.py
@@ -1,3 +1,19 @@
+from io import BytesIO
+from PIL import Image
+
+from django.core.files import File
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('name',)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
+
