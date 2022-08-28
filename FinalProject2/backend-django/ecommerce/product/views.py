260046b2a52c3ffa90d929e1073c99460c7143db
Nadim <nahlenadim@gmail.com>
2022-08-28 14:41:02 +0300

    Backend-Django Import rest_framework

--- FinalProject2/backend-django/ecommerce/product/views.py
+++ FinalProject2/backend-django/ecommerce/product/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
