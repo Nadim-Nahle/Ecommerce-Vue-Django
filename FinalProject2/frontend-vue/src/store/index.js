b8b30017b88497abd1541f94671b27672a35cf14
Nadim <nahlenadim@gmail.com>
2022-08-28 15:50:30 +0300

    Frontend-Vue Create- addtocart  function

--- FinalProject2/frontend-vue/src/store/index.js
+++ FinalProject2/frontend-vue/src/store/index.js
@@ -26,6 +26,16 @@ export default createStore({
         state.isAuthenticated = false
       }
     },
+    addToCart(state, item) {
+      const exists = state.cart.items.filter(i => i.product.id === item.product.id)
+      if (exists.length) {
+        exists[0].quantity = parseInt(exists[0].quantity) + parseInt(item.quantity)
+      } else {
+        state.cart.items.push(item)
+      }
+
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    },
 
   },
   actions: {
