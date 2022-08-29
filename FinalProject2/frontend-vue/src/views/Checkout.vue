a46db9ecf19ceeedd8c9d25bf1276a7ece9dc5e8
Nadim <nahlenadim@gmail.com>
2022-08-29 10:21:14 +0300

    Frontend-Vue Create- Checkout Errors

--- FinalProject2/frontend-vue/src/views/Checkout.vue
+++ FinalProject2/frontend-vue/src/views/Checkout.vue
@@ -137,16 +137,8 @@ export default {
     };
   },
   mounted() {
-    document.title = "Checkout | Djackets";
+    document.title = "Checkout | LeVehicle";
     this.cart = this.$store.state.cart;
-    if (this.cartTotalLength > 0) {
-      this.stripe = Stripe(
-        "pk_test_51H1HiuKBJV2qfWbD2gQe6aqanfw6Eyul5PO2KeOuSRlUMuaV4TxEtaQyzr9DbLITSZweL7XjK3p74swcGYrE2qEX00Hz7GmhMI"
-      );
-      const elements = this.stripe.elements();
-      this.card = elements.create("card", { hidePostalCode: true });
-      this.card.mount("#card-element");
-    }
   },
   methods: {
     getItemTotal(item) {
@@ -175,54 +167,6 @@ export default {
       if (this.place === "") {
         this.errors.push("The place field is missing!");
       }
-      if (!this.errors.length) {
-        this.$store.commit("setIsLoading", true);
-        this.stripe.createToken(this.card).then((result) => {
-          if (result.error) {
-            this.$store.commit("setIsLoading", false);
-            this.errors.push(
-              "Something went wrong with Stripe. Please try again"
-            );
-            console.log(result.error.message);
-          } else {
-            this.stripeTokenHandler(result.token);
-          }
-        });
-      }
-    },
-    async stripeTokenHandler(token) {
-      const items = [];
-      for (let i = 0; i < this.cart.items.length; i++) {
-        const item = this.cart.items[i];
-        const obj = {
-          product: item.product.id,
-          quantity: item.quantity,
-          price: item.product.price * item.quantity,
-        };
-        items.push(obj);
-      }
-      const data = {
-        first_name: this.first_name,
-        last_name: this.last_name,
-        email: this.email,
-        address: this.address,
-        zipcode: this.zipcode,
-        place: this.place,
-        phone: this.phone,
-        items: items,
-        stripe_token: token.id,
-      };
-      await axios
-        .post("/api/v1/checkout/", data)
-        .then((response) => {
-          this.$store.commit("clearCart");
-          this.$router.push("/cart/success");
-        })
-        .catch((error) => {
-          this.errors.push("Something went wrong. Please try again");
-          console.log(error);
-        });
-      this.$store.commit("setIsLoading", false);
     },
   },
   computed: {
