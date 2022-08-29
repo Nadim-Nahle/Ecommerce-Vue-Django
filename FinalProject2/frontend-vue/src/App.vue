a0b1b940e6a462ea5cf89adfd9175f061514a8eb
Nadim <nahlenadim@gmail.com>
2022-08-29 09:23:33 +0300

    Frontend-Vue Create- Search Bar

--- FinalProject2/frontend-vue/src/App.vue
+++ FinalProject2/frontend-vue/src/App.vue
@@ -23,6 +23,29 @@
         id="navbar-menu"
         v-bind:class="{ 'is-active': showMobileMenu }"
       >
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <form method="get" action="/search">
+              <div class="field has-addons">
+                <div class="control">
+                  <input
+                    type="text"
+                    class="input"
+                    placeholder="search here"
+                    name="query"
+                  />
+                </div>
+                <div class="control">
+                  <button class="button is-link">
+                    <span class="icon">
+                      <i class="fas fa-search"></i>
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
         <div class="navbar-end">
           <router-link to="/cars" class="navbar-item">Cars</router-link>
           <router-link to="/motorcycles" class="navbar-item"
