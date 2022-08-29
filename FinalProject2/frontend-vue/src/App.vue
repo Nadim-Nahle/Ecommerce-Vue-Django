bf709072dd4299264d053e061e98f6f59bf62863
Nadim <nahlenadim@gmail.com>
2022-08-29 09:48:41 +0300

    Frontend-Vue Link- Login top Backend

--- FinalProject2/frontend-vue/src/App.vue
+++ FinalProject2/frontend-vue/src/App.vue
@@ -89,6 +89,7 @@
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
@@ -100,6 +101,13 @@ export default {
   },
   beforeCreate() {
     this.$store.commit("initializeStore");
+
+    const token = this.$store.state.token;
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = "Token " + token;
+    } else {
+      axios.defaults.headers.common["Authorization"] = "";
+    }
   },
   mounted() {
     this.cart = this.$store.state.cart;
