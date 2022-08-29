7be8c41eb5ffa4594247e5964fb9b7c626061051
Nadim <nahlenadim@gmail.com>
2022-08-29 10:06:11 +0300

    Frontend-Vue Create- Checkout Link

--- FinalProject2/frontend-vue/src/views/Checkout.vue
+++ FinalProject2/frontend-vue/src/views/Checkout.vue
@@ -114,3 +114,128 @@
     </div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Checkout",
+  data() {
+    return {
+      cart: {
+        items: [],
+      },
+      stripe: {},
+      card: {},
+      first_name: "",
+      last_name: "",
+      email: "",
+      phone: "",
+      address: "",
+      zipcode: "",
+      place: "",
+      errors: [],
+    };
+  },
+  mounted() {
+    document.title = "Checkout | Djackets";
+    this.cart = this.$store.state.cart;
+    if (this.cartTotalLength > 0) {
+      this.stripe = Stripe(
+        "pk_test_51H1HiuKBJV2qfWbD2gQe6aqanfw6Eyul5PO2KeOuSRlUMuaV4TxEtaQyzr9DbLITSZweL7XjK3p74swcGYrE2qEX00Hz7GmhMI"
+      );
+      const elements = this.stripe.elements();
+      this.card = elements.create("card", { hidePostalCode: true });
+      this.card.mount("#card-element");
+    }
+  },
+  methods: {
+    getItemTotal(item) {
+      return item.quantity * item.product.price;
+    },
+    submitForm() {
+      this.errors = [];
+      if (this.first_name === "") {
+        this.errors.push("The first name field is missing!");
+      }
+      if (this.last_name === "") {
+        this.errors.push("The last name field is missing!");
+      }
+      if (this.email === "") {
+        this.errors.push("The email field is missing!");
+      }
+      if (this.phone === "") {
+        this.errors.push("The phone field is missing!");
+      }
+      if (this.address === "") {
+        this.errors.push("The address field is missing!");
+      }
+      if (this.zipcode === "") {
+        this.errors.push("The zip code field is missing!");
+      }
+      if (this.place === "") {
+        this.errors.push("The place field is missing!");
+      }
+      if (!this.errors.length) {
+        this.$store.commit("setIsLoading", true);
+        this.stripe.createToken(this.card).then((result) => {
+          if (result.error) {
+            this.$store.commit("setIsLoading", false);
+            this.errors.push(
+              "Something went wrong with Stripe. Please try again"
+            );
+            console.log(result.error.message);
+          } else {
+            this.stripeTokenHandler(result.token);
+          }
+        });
+      }
+    },
+    async stripeTokenHandler(token) {
+      const items = [];
+      for (let i = 0; i < this.cart.items.length; i++) {
+        const item = this.cart.items[i];
+        const obj = {
+          product: item.product.id,
+          quantity: item.quantity,
+          price: item.product.price * item.quantity,
+        };
+        items.push(obj);
+      }
+      const data = {
+        first_name: this.first_name,
+        last_name: this.last_name,
+        email: this.email,
+        address: this.address,
+        zipcode: this.zipcode,
+        place: this.place,
+        phone: this.phone,
+        items: items,
+        stripe_token: token.id,
+      };
+      await axios
+        .post("/api/v1/checkout/", data)
+        .then((response) => {
+          this.$store.commit("clearCart");
+          this.$router.push("/cart/success");
+        })
+        .catch((error) => {
+          this.errors.push("Something went wrong. Please try again");
+          console.log(error);
+        });
+      this.$store.commit("setIsLoading", false);
+    },
+  },
+  computed: {
+    cartTotalPrice() {
+      return this.cart.items.reduce((acc, curVal) => {
+        return (acc += curVal.product.price * curVal.quantity);
+      }, 0);
+    },
+    cartTotalLength() {
+      return this.cart.items.reduce((acc, curVal) => {
+        return (acc += curVal.quantity);
+      }, 0);
+    },
+  },
+};
+</script>
