867ba0ee505550343a234c73c2f44ef9f6d4c671
Nadim <nahlenadim@gmail.com>
2022-08-29 10:06:24 +0300

    Frontend-Vue Create- Checkout Route

--- FinalProject2/frontend-vue/src/router/index.js
+++ FinalProject2/frontend-vue/src/router/index.js
@@ -8,6 +8,7 @@ import Cart from '../views/Cart.vue'
 import SignUp from '../views/SignUp.vue'
 import Login from '../views/Login.vue'
 import MyAccount from '../views/MyAccount.vue'
+import Checkout from '../views/Checkout.vue'
 const routes = [
   {
     path: '/',
@@ -38,6 +39,14 @@ const routes = [
     path: '/my-account',
     name: 'MyAccount',
     component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/cart/checkout',
+    name: 'Checkout',
+    component: Checkout,
     meta: {
       requireLogin: true
     }
